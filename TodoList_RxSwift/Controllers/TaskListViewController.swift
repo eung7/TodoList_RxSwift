7662e49d44d4a5315c9c5b1f2ac557a27f5b9d9a
eung7 <wannabeeung@gmail.com>
2022-06-16 00:37:14 +0900

    [Add] Adding Tasks Using BehaviorRelay

--- TodoList_RxSwift/Controllers/TaskListViewController.swift
+++ TodoList_RxSwift/Controllers/TaskListViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var prioritySegementedControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
     let disposeBag = DisposeBag()
+    private var tasks = BehaviorRelay<[Task]>(value: [])
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +26,7 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.value.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -37,7 +39,10 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
               let addTVC = navC.viewControllers.first as? AddTaskViewController else { return }
         addTVC.taskSubjectObservable
             .subscribe(onNext: { task in
-                print(task)
+                let priority = Priority(rawValue: self.prioritySegementedControl.selectedSegmentIndex - 1)
+                var existingTasks = self.tasks.value
+                existingTasks.append(task)
+                self.tasks.accept(existingTasks)
             }).disposed(by: disposeBag)
     }
 }
